890c5639d598411b652bd08efcec672d694e8b8b
himanshiiiii <himanshi.gupta@tothenew.com>
2018-02-26 15:39:50 +0530

    changes committed

---
+++ test/Groovy/AdditionSpec.groovy
@@ -0,0 +1,40 @@
+import spock.lang.Specification
+
+class AdditionSpec extends Specification {
+    def addition
+    void setup() {
+        addition = new Addition();
+    }
+
+    void "canary test"(){
+        println "this is canary test"
+        expect:
+        true
+    }
+    void "testSumInts"() {
+
+        setup:
+        def a = 10
+        
+        and:
+        def b = 20
+
+        when:
+        c = addition.add(a, b)
+
+        then:
+        c == 30
+    }
+
+    void "testSumStrings"() {
+        given:
+        def a = "Hello"
+        
+        and:
+        def b = "World"
+
+        expect:
+        addition.sum(a, b) == "HelloWorld"
+    }
+
+}
